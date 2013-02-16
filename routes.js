b857535af5ddb23e551a631b5be6131a395aeec7
Ai Shimogori <ai.shimogori@gmail.com>
2013-02-16 19:09:38 +0900

    add post create

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 var model = require('./lib/model');
+var crypto = require('crypto');
 
 /**
  * get home page
@@ -18,7 +19,27 @@ exports.createNew = function(req, res){
  * accept create request and validate
  */
 exports.create = function(req, res) {
-  res.render('index.html', { title: 'Express', action: 'create' });
+  var d = new Date();
+  var dt = d.getTime();
+  uniq_code = crypto.createHash('md5').update(dt+req.subject).digist("hex");
+
+  RequestListModel = new model.RequestList();
+  RequestListModel.subject = req.params.subject;
+  RequestListModel.code = uniq_code;
+  RequestListModel.items = [];
+
+  for(var i = 0; i < req.items.length; i++) {
+  	RequestItemModel = new model.RequestItem();
+  	RequestItemModel.subject = req.items[i].subject;
+  	RequestItemModel.overview = req.items[i].overview;
+  	RequestItemModel.cost = req.items[i].cost;
+  	RequestItemModel.type = req.items[i]type;
+  	RequestItemModel.source_url = req.items[i].source_url;
+
+  	RequestListModel.items.push(RequestItemModel); 
+  }
+
+  res.render('show.html', { title: 'Express' });
 };
 
 /**
